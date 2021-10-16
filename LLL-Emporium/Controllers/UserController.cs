f2076c618c89225bb40e8441cc5ac6dbb4eab84d
Jonathon Joyner <jonathonandon@gmail.com>
2021-10-16 10:51:36 -0500

    add get method for users

---
+++ LLL-Emporium/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LLL_Emporium.DataAccess;
+using LLL_Emporium.Models;
+
+namespace LLL_Emporium.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class UserController : ControllerBase
+
+    { 
+        private UserRepository _userRepository;
+
+            public UserController(UserRepository userRepo)
+        {
+            _userRepository = userRepo;
+        }
+
+
+        [HttpGet]
+        public IActionResult GetAllUsers()
+        {
+            var result = _userRepository.GetAll();
+            if (result.Count() >= 0)
+            {
+                return Ok(result);
+            }
+            else return NotFound("No users");
+        }
+    }
+}
---
+++ LLL-Emporium/DataAccess/UserRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+using LLL_Emporium.Models;
+
+namespace LLL_Emporium.DataAccess
+{
+    public class UserRepository
+    {
+        readonly string _connectionString;
+
+        public UserRepository(IConfiguration config)
+        {
+            _connectionString = config.GetConnectionString("LLL-Emporium");
+        }
+
+        internal IEnumerable<User> GetAll()
+        {
+            using var db = new SqlConnection(_connectionString);
+
+            var users = db.Query<User>(@"Select * From Users");
+            return users;
+        }
+    }
+}
