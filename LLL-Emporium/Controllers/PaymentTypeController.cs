26f65035eddafdc2072ed8011d4e0091d85fee23
tsekeres <twsekeres@gmail.com>
2021-10-16 13:53:43 -0500

    Getting started on payment types CRUD

---
+++ LLL-Emporium/Controllers/PaymentTypeController.cs
@@ -0,0 +1,18 @@
+﻿using LLL_Emporium.DataAccess;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LLL_Emporium.Controllers
+{
+    [Route("api/paymenttypes")]
+    [ApiController]
+    public class PaymentTypeController : ControllerBase
+    {
+        private PaymentTypeRepository paymentTypeRepository;
+
+    }
+}
---
+++ LLL-Emporium/DataAccess/PaymentTypeRepository.cs
@@ -0,0 +1,29 @@
+﻿using Dapper;
+using LLL_Emporium.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LLL_Emporium.DataAccess
+{
+    public class PaymentTypeRepository
+    {
+        readonly string _connectionString;
+
+        public PaymentTypeRepository(IConfiguration config)
+        {
+            _connectionString = config.GetConnectionString("LLL-Emporium");
+        }
+
+        internal IEnumerable<PaymentType> GetAllPaymentTypes()
+        {
+            using var db = new SqlConnection(_connectionString);
+            var sql = @"SELECT * FROM PaymentTypes";
+            var result = db.Query<PaymentType>(sql);
+            return result;
+        }
+    }
+}
