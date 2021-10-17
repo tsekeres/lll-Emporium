af8b30a5147888313f283706219a3e1b05151800
John Maple <jmaple@bellsouth.net>
2021-10-16 21:20:38 -0500

    updating order data

--- SQL/SampleData.sql
+++ SQL/SampleData.sql
@@ -288,7 +288,7 @@ INSERT INTO Orders ( CustomerId, ShippingAddress, ShippingCity,
 					 ShippingState, ShippingZip, OrderDate, Completed )
 VALUES ((SELECT Id FROM Users WHERE FirstName = 'Honey-Rae'), '1000 Something', 'Boston',
         'MA', '02113', CURRENT_TIMESTAMP, '0'),
-		((SELECT Id FROM Users WHERE FirstName = 'John'), '710 Seven Mile Ct','Nashville',
+		((SELECT Id FROM Users WHERE FirstName = 'John'), '123 Somewhere','Nashville',
         'TN', '37211', CURRENT_TIMESTAMP, '0')
 
 INSERT INTO OrderLines ( OrderId, ProductId, UnitPrice, Quantity, Discount )
