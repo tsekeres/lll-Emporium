8ffe0d0775e1540726d48c9a7cba49db95941588
John Maple <jmaple@bellsouth.net>
2021-10-18 19:55:21 -0500

    adding get, get by Id for role types

---
+++ LLL-Emporium/Controllers/RoleTypeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LLL_Emporium.DataAccess;
+using LLL_Emporium.Models;
+
+
+namespace LLL_Emporium.Controllers
+{
+    [Route("api/roleTypes")]
+    [ApiController]
+    public class RoleTypeController : ControllerBase
+    {
+        private RoleTypeRepository _roleTypeRepository;
+
+        public RoleTypeController(RoleTypeRepository roleTypeRepo)
+        {
+            _roleTypeRepository = roleTypeRepo;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllRoleTypes()
+        {
+            var result = _roleTypeRepository.GetAllRoleTypes();
+            if (result.Count() > 0)
+            {
+                return Ok(result);
+            }
+            else return NotFound("No role types.");
+        }
+    }
+}
---
+++ LLL-Emporium/DataAccess/RoleTypeRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Dapper;
+using LLL_Emporium.Models;
+
+
+namespace LLL_Emporium.DataAccess
+{
+    public class RoleTypeRepository
+    {
+        readonly string _connectionString;
+        public RoleTypeRepository(IConfiguration config)
+        {
+            _connectionString = config.GetConnectionString("LLL-Emporium");
+        }
+
+        internal IEnumerable<RoleType> GetAllRoleTypes()
+        {
+            using var db = new SqlConnection(_connectionString);
+            var sql = @"SELECT * FROM RoleTypes";
+            var result = db.Query<RoleType>(sql);
+            return result;
+        }
+
+        internal RoleType GetRoleTypeById(Guid roleId)
+        {
+            using var db = new SqlConnection(_connectionString);
+            var sql = @"SELECT * FROM RoleTypes 
+                        WHERE Id = @Id";
+            var parameter = new
+            {
+                Id = roleId
+            };
+
+            var result = db.QuerySingleOrDefault<RoleType>(sql, parameter);
+            return result;
+            }
+        }
+    }
+}
