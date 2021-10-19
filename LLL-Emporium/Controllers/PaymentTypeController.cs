937a481bfc2874102d4290bff9ecd4216ba0ad54
tsekeres <twsekeres@gmail.com>
2021-10-18 21:16:50 -0500

    Payment Type CRUD achieved.

--- LLL-Emporium/Controllers/PaymentTypeController.cs
+++ LLL-Emporium/Controllers/PaymentTypeController.cs
@@ -1,4 +1,5 @@
 ﻿using LLL_Emporium.DataAccess;
+using LLL_Emporium.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -28,6 +29,53 @@ namespace LLL_Emporium.Controllers
                 return Ok(result);
             }
             else return NotFound("No payment types.");
+        }
+
+        [HttpGet("{paymentTypeId}")]
+        public IActionResult GetPaymentTypeById(Guid paymentTypeId)
+        {
+            var result = _paymentTypeRepository.GetPaymentTypeById(paymentTypeId);
+            if (result != null)
+            {
+                return Ok(result);
+            }
+            else return NotFound("Payment Type not found");
+        }
+
+        [HttpPost]
+        public IActionResult CreatePaymentType(PaymentType paymentType)
+        {
+            var result = _paymentTypeRepository.AddPaymentType(paymentType);
+            if (result.Equals(Guid.Empty))
+            {
+                return BadRequest("Payment Type Not Added");
+            }
+            else
+            {
+                return Created($"/api/paymentTypes/{result}", result);
+            }
+        }
+
+        [HttpDelete("{paymentTypeId}")]
+        public IActionResult DeletePaymentType(Guid paymentTypeId)
+        {
+            var result = _paymentTypeRepository.DeletePaymentType(paymentTypeId);
+            if (result)
+            {
+                return Ok($"Payment Type with Id {paymentTypeId} was deleted");
+            }
+            else return NotFound($"Payment Type with Id {paymentTypeId} not found for deletion");
+        }
+
+        [HttpPatch("{paymentTypeId}")]
+        public IActionResult PatchPaymentType(Guid paymentTypeId, PaymentType paymentType)
+        {
+            var result = _paymentTypeRepository.UpdatePaymentType(paymentTypeId, paymentType);
+            if (result)
+            {
+                return Ok($"Payment Type with id {paymentTypeId} has been updated");
+            }
+            else return BadRequest($"Payment Type with id {paymentTypeId} not updated");
         }
 
     }
