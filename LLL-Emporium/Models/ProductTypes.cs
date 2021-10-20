5b2fc13ebd2b8bd44d32d0797c0c03c4bd492c7b
Jonathon Joyner <jonathonandon@gmail.com>
2021-10-20 18:48:40 -0500

    add all changes for crud

---
+++ LLL-Emporium/Models/ProductTypes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LLL_Emporium.Models
+{
+    public class ProductTypes
+    {
+        public Guid Id { get; set; }
+        public Guid CategoryId { get; set; }
+        public string TypeName { get; set; }
+        public string ProductTypeImageUrl { get; set; }
+    }
+}
