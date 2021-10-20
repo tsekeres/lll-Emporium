712f32f0a1716144dd73f159f59cde4bfcefda76
John Maple <jmaple@bellsouth.net>
2021-10-19 19:10:53 -0500

    starting orders repository with all the data

---
+++ LLL-Emporium/DataAccess/OrderWithDetailsRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using Dapper;
+using LLL_Emporium.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace LLL_Emporium.DataAccess
+{
+    public class OrderWithDetailsRepository
+    {
+        readonly string _connectionString;
+
+        OrderWithDetailsRepository(IConfiguration config)
+        {
+            _connectionString = config.GetConnectionString("LLL-Emporium");
+        }
+
+
+    }
+}
---
+++ LLL-Emporium/Models/OrderWithDetails.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LLL_Emporium.Models
+{
+    public class OrderWithDetails
+    {
+        public Order Order { get; set; }
+        public OrderLineMultiple LineItems { get; set; }
+    }
+}
