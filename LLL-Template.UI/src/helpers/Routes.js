b9087a67936ed5d9cfd51a2f944fcf4b3c5df5e3
Honey-Rae Swan <honeyraeswan@gmail.com>
2021-10-25 12:52:19 -0400

    fixed eslint errors to allow app to render

--- LLL-Template.UI/src/helpers/Routes.js
+++ LLL-Template.UI/src/helpers/Routes.js
@@ -3,12 +3,12 @@ import { Route, Switch, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 const PrivateRoute = ({ component: Component, user, ...rest }) => {
-  const routeChecker = (taco) =>
-    user ? (
+  // eslint-disable-next-line no-confusing-arrow
+  const routeChecker = (taco) => user ? (
       <Component {...taco} user={user} />
-    ) : (
+  ) : (
       <Redirect to={{ pathname: '/', state: { from: taco.location } }} />
-    );
+  );
   return <Route {...rest} render={(props) => routeChecker(props)} />;
 };
 
@@ -21,22 +21,12 @@ function Routes({ user }) {
   return (
     <div>
       <Switch>
-        <Route exact path="/" user={user} component={Home} />
+        <Route exact path="/" user={user} />
         <PrivateRoute
-          exact
-          path="/"
-          user={user}
-          component={() => (
-            <ViewName user={user} />
-          )}
         />
         <PrivateRoute
-          exact
-          path="/"
-          user={user}
-          component={() => <ViewNAme user={user} />}
         />
-        <Route path="*" component={Home} />
+        <Route path="*" />
       </Switch>
     </div>
   );
