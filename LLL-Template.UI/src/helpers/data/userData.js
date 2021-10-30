f27f4ed0a0397abd312e7581ac1fbd0eab851f06
Jonathon Joyner <jonathonandon@gmail.com>
2021-10-30 11:46:25 -0500

    add these changes

--- LLL-Template.UI/src/helpers/data/userData.js
+++ LLL-Template.UI/src/helpers/data/userData.js
@@ -1,24 +1,13 @@
 import axios from 'axios';
 import LLLConfig from '../apiKeys';
 
-<<<<<<< HEAD
 const dbUrl = LLLConfig.baseUrl;
 
+// took outmerge conflicts
 const getUsers = () => new Promise((resolve, reject) => {
   axios.get(`${dbUrl}/api/users`)
-    .then((userList) => { console.warn(userList); resolve(userList.data); })
+    .then((userList) => resolve(userList.data))
     .catch((err) => reject(err));
 });
 
 export default getUsers;
-=======
-const dbURL = LLLConfig.baseUrl;
-
-const getAllUsers = () => new Promise((resolve, reject) => {
-  axios.get(`${dbURL}/api/users`)
-    .then((response) => resolve(response.data))
-    .catch((error) => reject(error));
-});
-
-export default getAllUsers;
->>>>>>> main
