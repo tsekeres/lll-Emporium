83406e5c6fc34ef2af7660537230974849108efc
Jonathon Joyner <jonathonandon@gmail.com>
2021-10-30 11:37:20 -0500

    add all read for user

--- LLL-Template.UI/src/components/Cards/UserCards/UserCardElements.js
+++ LLL-Template.UI/src/components/Cards/UserCards/UserCardElements.js
@@ -1,15 +1,34 @@
 import styled from 'styled-components';
 
 export const CardBio = styled.div` 
-width: 40em;
+width: 196px;
+height: 231px;
+left: 390px;
+top: 145px;
+
+font-family: Open Sans;
+font-style: normal;
+font-weight: 300;
+font-size: 14px;
+line-height: 19px;
+display: flex;
+align-items: right;
+text-align: right;
 `;
 
 export const Card = styled.div`
 width: 50em;
+display: flex;
+align-items: flex-start;
+display: wrap;
 `;
 
 export const CardImg = styled.img`
-width: 20em;
+width: 271px;
+border-radius: 50%;
+height: 276.02px;
+left: 85px;
+top: 142px;
 `;
 
 export const CardBody = styled.div` 
